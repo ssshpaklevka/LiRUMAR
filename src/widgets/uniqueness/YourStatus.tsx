b08d417e348d12f7ab88d63a7c9147e733119cfb
ssshpaklevka <apactop2004@gmail.com>
2025-01-19 19:39:49 +0700

    fix(YourStatus)

--- src/widgets/uniqueness/YourStatus.tsx
+++ src/widgets/uniqueness/YourStatus.tsx
@@ -6,9 +6,9 @@ const YourStatus: FC = () => {
   return (
     <div className="flex flex-col relative">
       <div className="h-[450px] flex items-center flex-row md:pt-[96px] gap-24">
-        <p className="text-[33px] leading-[36px] max-w-[380px] md:text-[60px] md:leading-[66px] md:w-[660px] xl:text-[45px] xl:leading-[50px] xl:w-[600px] 3xl:text-[79px] 3xl:leading-[87px] 3xl:w-[1060px] absolute z-10">
+        <p className="text-[33px] leading-[36px] max-w-[380px] md:text-[60px] md:leading-[66px] md:max-w-[660px] xl:text-[45px] xl:leading-[50px] xl:max-w-[600px] 3xl:text-[79px] 3xl:leading-[87px] 3xl:max-w-[1060px] absolute z-10">
           <span className="subfont text-[38px] leading-[42px] md:text-[69px] md:leading-[76px] xl:text-[50px] xl:leading-[55px] 3xl:text-[96px] 3xl:leading-[105px] ">
-            -уникальность,{' '}
+            — уникальность,{' '}
           </span>
           которая подчеркивает ваш статус
         </p>
@@ -17,7 +17,7 @@ const YourStatus: FC = () => {
           src={'/img/uniqueness/shoes.webp'}
           width={365}
           height={417}
-          className="grayscale absolute right-0 3xl:mr-[250px] top-0 3xl:top-6 z-0 w-[267px] h-[305px] sm:w-[390px] sm:h-[420px] xl:w-[490px] xl:h-[559px] 3xl:w-[584px] 3xl:h-[667px]"
+          className="filter brightness-50 grayscale absolute right-0 3xl:mr-[250px] top-0 3xl:top-6 z-0 w-[267px] h-[305px] sm:w-[390px] sm:h-[420px] xl:w-[490px] xl:h-[580px] 3xl:w-[584px] 3xl:h-[667px]"
           alt="shoes"
         />
       </div>
@@ -27,7 +27,7 @@ const YourStatus: FC = () => {
           width={311}
           height={357}
           alt="sneakers"
-          className="grayscale w-[171px] h-[197px] sm:w-[290px] sm:h-[320px] xl:w-[313px] xl:h-[360px] 3xl:w-[497px] 3xl:h-[571px]"
+          className="filter brightness-50 grayscale w-[171px] h-[197px] sm:w-[290px] sm:h-[320px] xl:w-[313px] xl:h-[360px] 3xl:w-[497px] 3xl:h-[571px]"
         />
       </div>
     </div>
