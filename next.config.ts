b58527a324e62bba7d404f1c59f4b4e8f169d227
clownlessmode <clownessmode@bk.ru>
2025-07-11 14:12:36 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,19 @@ const nextConfig: NextConfig = {
   },
   images: {
     domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+        pathname: '/uploads/**',
+      },
+    ],
   },
 };
 
